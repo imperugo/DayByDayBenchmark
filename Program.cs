265a11ab793dc91c04294c5f11a392bab8767345
lbellavia <lbellavia@technogym.com>
2019-10-01 14:01:22 +0200

    Problem with benchmark solved. Now it works

--- Program.cs
+++ Program.cs
@@ -1,18 +1,14 @@
-﻿using BenchmarkDotNet.Configs;
-using BenchmarkDotNet.Jobs;
+﻿using System;
 using BenchmarkDotNet.Running;
-using BenchmarkDotNet.Toolchains.InProcess.Emit;
 
-namespace Benchmark {
-    class Program {
-
-        public static void Main (string[] args) {
-            var config = ManualConfig
-                .Create (DefaultConfig.Instance);
-            config.Add (Job.ShortRun.With (InProcessEmitToolchain.Instance));
-            
-            var summary = BenchmarkRunner.Run<BenchmarkLinq> ();
-
+namespace Benchmark
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+            Console.ReadLine();
         }
     }
 }
