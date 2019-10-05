e9aabe0c72147bdd5d36973a46940dade44c184a
Luca <luca.bellavia.dev@gmail.com>
2019-10-05 14:11:21 +0200

    Bug fixing (deleted logs on benchmark methods) - Stable version

--- Services/Implementations/BenchmarkLinq.cs
+++ Services/Implementations/BenchmarkLinq.cs
@@ -5,21 +5,16 @@ using Benchmark.Models.Implementations;
 using Benchmark.Services.Abstractions;
 using BenchmarkDotNet.Attributes;
 using Bogus;
-using Microsoft.Extensions.Logging;
 
 namespace Benchmark {
+
     [MemoryDiagnoser]
     [CsvExporter]
     [RPlotExporter]
     public class BenchmarkLinq : IBenchmarkLinq {
         private readonly List<Customer> customers;
-        private readonly ILogger<BenchmarkLinq> logger;
 
-        public BenchmarkLinq (ILogger<BenchmarkLinq> logger) {
-
-            this.logger = logger;
-
-            logger.LogInformation ("Start of the chain process..");
+        public BenchmarkLinq () {
 
             int Ids = 0;
             Faker<Customer> test = new Faker<Customer> ()
@@ -31,7 +26,6 @@ namespace Benchmark {
 
             customers = test.Generate (500000);
 
-            logger.LogInformation ("End of the chain process.");
         }
 
         public void Start () {
@@ -45,72 +39,53 @@ namespace Benchmark {
 
         [Benchmark (Baseline = true)]
         public void Linq_1000 () {
-
-            logger.LogInformation ($"Starting first call: {nameof(Linq_1000)}");
-
             _ = customers
                 .Take (1000)
                 .All (x => expensiveOperation (x.FirstName + x.LastName + x.EmailAddress + x.Id + x.TelephoneNumbers.FirstOrDefault ()));
-
-            logger.LogInformation ($"First call finished: {nameof(Linq_1000)}");
         }
 
         [Benchmark]
         public void PLinq_1000 () {
 
-            logger.LogInformation ($"Starting second call: {nameof(PLinq_1000)}");
-
             _ = customers.AsParallel ()
                 .Take (1000)
                 .All (x => expensiveOperation (x.FirstName + x.LastName + x.EmailAddress + x.Id + x.TelephoneNumbers.FirstOrDefault ()));
-
-            logger.LogInformation ($"Second call finished: {nameof(Linq_1000)}");
+        
         }
 
         [Benchmark]
         public void Linq_100000 () {
-            logger.LogInformation ($"Starting third call: {nameof(Linq_100000)}");
 
             _ = customers
                 .Take (100000)
                 .All (x => expensiveOperation (x.FirstName + x.LastName + x.EmailAddress + x.Id + x.TelephoneNumbers.FirstOrDefault ()));
 
-            logger.LogInformation ($"Third call finished: {nameof(Linq_100000)}");
         }
 
         [Benchmark]
         public void PLinq_100000 () {
 
-            logger.LogInformation ($"Starting fourth call: {nameof(PLinq_100000)}");
-
             _ = customers.AsParallel ()
                 .Take (100000)
                 .All (x => expensiveOperation (x.FirstName + x.LastName + x.EmailAddress + x.Id + x.TelephoneNumbers.FirstOrDefault ()));
 
-            logger.LogInformation ($"Fourth call finished: {nameof(PLinq_100000)}");
         }
 
         [Benchmark]
         public void Linq_500000 () {
 
-            logger.LogInformation ($"Starting fifth call: {nameof(Linq_500000)}");
-
             _ = customers.Take (500000)
                 .All (x => expensiveOperation (x.FirstName + x.LastName + x.EmailAddress + x.Id + x.TelephoneNumbers.FirstOrDefault ()));
 
-            logger.LogInformation ($"Fifth call finished: {nameof(Linq_500000)}");
         }
 
         [Benchmark]
         public void PLinq_500000 () {
 
-            logger.LogInformation ($"Starting sixth call: {nameof(Linq_500000)}");
-
             _ = customers.AsParallel ()
                 .Take (500000)
                 .All (x => expensiveOperation(x.FirstName + x.LastName + x.EmailAddress + x.Id + x.TelephoneNumbers.FirstOrDefault()));
 
-            logger.LogInformation ($"Sixth call finished: {nameof(Linq_500000)}");
         }
 
         private bool expensiveOperation (string word) {
